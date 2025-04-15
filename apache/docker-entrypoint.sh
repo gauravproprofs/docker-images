d371245cd3ddd9b80d36ac4585092413fcd4ee87
Gaurav Bhatt <sadmin@srv01.proprofs.com>
2025-04-15 02:44:45 -0500

    remove dir

--- apache/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Start Sendmail in the background
-service sendmail start
-
-# Start Apache in the foreground
-exec apache2-foreground
--- mailserver/set-hostname.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set the hostname to "smtpserver"
-echo "mail.proprofs.io" > /etc/hostname
-echo "127.0.0.1 mail.proprofs.io mail" >> /etc/hosts
-
-# Execute the passed command
-exec "$@"
